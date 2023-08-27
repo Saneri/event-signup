b1abbc88f1296ffda9536217bbd6566f703780fc
Santeri Suitiala <santeri.suitiala@almamedia.fi>
2023-08-27 22:12:58 +0300

    add createTable to db init

--- backend/src/initDynamodb.ts
+++ backend/src/initDynamodb.ts
@@ -1,4 +1,4 @@
-import { DynamoDBClient, ListTablesCommand } from '@aws-sdk/client-dynamodb';
+import { CreateTableCommand, DynamoDBClient, ListTablesCommand } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 
 const dynamo = new DynamoDBClient({
@@ -7,7 +7,28 @@ const dynamo = new DynamoDBClient({
 });
 const client = DynamoDBDocument.from(dynamo);
 
-// TODO: add items to local db
+const createTable = async (tableName: string) => {
+    const params = {
+        TableName: tableName,
+        AttributeDefinitions: [{ AttributeName: 'id', AttributeType: 'S' }],
+        KeySchema: [{ AttributeName: 'id', KeyType: 'HASH' }],
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1,
+        },
+    };
+    const command = new CreateTableCommand(params);
+
+    try {
+        const result = await client.send(command);
+        console.log('Table created successfully:', result);
+    } catch (err) {
+        console.error('Error creating table:', err);
+    }
+};
+
+createTable('YourTableName');
+
 (async () => {
     const result = await client.send(new ListTablesCommand({}));
     console.log(result);
