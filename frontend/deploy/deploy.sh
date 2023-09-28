a80d950ee42cae1370e1c8d80ac0340f41fa45a9
Santeri Suitiala <santeri.suitiala@almamedia.fi>
2023-09-28 07:36:31 +0300

    add S3 cloudformation deploy script

---
+++ frontend/deploy/deploy.sh
@@ -0,0 +1,12 @@
+readonly BUCKET_NAME=event-signup-dev
+readonly AWS_PROFILE=santeri-dev
+
+aws cloudformation deploy \
+    --profile ${AWS_PROFILE} \
+    --region eu-west-1 \
+    --template-file template.yaml \
+    --stack-name ${BUCKET_NAME}
+
+npm run build
+aws s3 sync --profile ${AWS_PROFILE} --delete ../dist/ s3://${BUCKET_NAME}
+
