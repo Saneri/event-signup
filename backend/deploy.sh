ec9e144e39236caccf380bac093b6d96c2d751ce
Santeri Suitiala <santeri.suitiala@almamedia.fi>
2023-10-01 18:09:16 +0300

    add authorization for Api Gateway

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,4 +4,5 @@ sam deploy  \
     --stack-name event-signup-backend \
     --profile santeri-dev \
     --capabilities CAPABILITY_IAM \
-    --no-confirm-changeset
+    --no-confirm-changeset \
+    --s3-bucket event-signup-backend-sam
