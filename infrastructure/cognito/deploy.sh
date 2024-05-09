a731b7d075d2193b431b72f52352c0e96b2318f2
Saneri <santeri.suitiala@gmail.com>
2024-05-09 15:51:09 +0300

    infra: separate cognito and dynamo, make username case insensitive

---
+++ infrastructure/cognito/deploy.sh
@@ -0,0 +1,7 @@
+readonly AWS_PROFILE=santeri-dev
+
+aws cloudformation deploy \
+    --profile ${AWS_PROFILE} \
+    --region eu-west-1 \
+    --template-file template.yaml \
+    --stack-name event-signup-cognito
