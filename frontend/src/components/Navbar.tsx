1e02dfa98981987ab767bcc7d2f3a4e6a30b7230
Saneri <santeri.suitiala@gmail.com>
2024-11-23 16:00:53 +0200

    replace logout button with user menu

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
 import { signOut } from "../auth/auth";
 import { useUser } from "../providers/UserProvider";
-import Button from "./common/Button";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
+import { Button } from "./ui/button";
 
 const Navbar = () => {
   const { user, fetchUser } = useUser();
@@ -23,10 +32,28 @@ const Navbar = () => {
           </span>
         </a>
         <ul className="flex flex-wrap items-center gap-4 text-lg">
-          {email && <li>{email}</li>}
           <li>
             {email ? (
-              <Button onClick={logout}>Logout</Button>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button className="flex items-center space-x-2">
+                    <Avatar className="h-6 w-6">
+                      <AvatarImage src={""} alt={email} />
+                      <AvatarFallback>{email.charAt(0)}</AvatarFallback>
+                    </Avatar>
+                    <span>{email}</span>
+                    <ChevronDown className="h-4 w-4 opacity-50" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuItem
+                    onSelect={logout}
+                    className="cursor-pointer p-2 hover:bg-gray-100"
+                  >
+                    Logout
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
             ) : (
               <Link to="/login">
                 <Button>Login</Button>
