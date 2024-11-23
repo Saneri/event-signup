fdb232b2c0dd566774f9073fdde4a32d193a9ef9
Saneri <santeri.suitiala@gmail.com>
2024-11-23 17:28:00 +0200

    use css variables in shadcn

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="bg-indigo-500 text-white sm:px-20">
+    <nav className="sm:px-20">
       <div className="flex flex-wrap items-center justify-between p-4">
         <a href="/">
           <span className="self-center text-2xl font-semibold">
