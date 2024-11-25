30c22de3a4172e19cb2fe0b981879b13532d86e9
Saneri <santeri.suitiala@gmail.com>
2024-11-25 08:07:40 +0200

    use shadcn button everywhere

--- frontend/src/components/login/AccountConfirmationForm.tsx
+++ frontend/src/components/login/AccountConfirmationForm.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
 import * as yup from "yup";
 import { confirmRegistration } from "../../auth/auth";
-import Button from "../common/Button";
 import FormError from "../common/FormError";
-import { useNavigate } from "react-router-dom";
 
 type Props = {
   emailToConfirm: string;
@@ -45,12 +46,7 @@ const AccountConfirmationForm = (props: Props) => {
     <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
       <form className="flex flex-col" onSubmit={formik.handleSubmit}>
         <label className="my-1">Confirmation code</label>
-        <input
-          className="shadow border rounded py-2 px-3 my-1 text-gray-700"
-          type="text"
-          name="code"
-          onChange={formik.handleChange}
-        />
+        <Input type="text" name="code" onChange={formik.handleChange} />
         <FormError error={formik.errors.code} />
         <Button className="mt-2" type="submit">
           Confirm email
