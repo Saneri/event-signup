86003f98f36021bbbee1301146eccc72b58a4a4d
Saneri <santeri.suitiala@gmail.com>
2025-11-05 11:14:10 +0200

    update tailwind from 3 to 4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vitest/config";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   envDir: "env",
   test: {
     globals: true,
